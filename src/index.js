2a2b7bfdcb6cf6eeedc4558be8c53df2f0792962
wander <wanderbit123@mail.ru>
2017-07-26 08:00:20 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 class HelloMessage extends React.Component {
+
+
+    static propTypes = {
+        button: React.PropTypes.string.isRequired
+    };
+    static defaultProps = {
+        button: 'default'
+    };
+    btnOnClick(){
+        console.log('test');
+    }
+
     render() {
-        return <div>Hello {this.props.name}</div>;
+
+        const divStyle = {
+            backgroundColor : 'blue',
+
+        };
+        return (
+            <div  style={divStyle} >
+                <h1 className="test">test</h1>
+                Hello {this.props.name}
+                <button onClick = {this.btnOnClick}>{this.props.button}</button>
+            </div>
+        );
     }
 }
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('time')
+    );
+}
 
-ReactDOM.render(<HelloMessage name="John" />, mountNode);
+setInterval(tick, 1000);
+
+ReactDOM.render(<HelloMessage name="John" /*button="clicks"*//>, document.getElementById('app'));
+
